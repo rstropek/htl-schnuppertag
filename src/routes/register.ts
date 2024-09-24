3f8c156c1cbe8f6b2dd5ba05ba7ac4e71e14db64
Rainer <rainer@software-architects.at>
2024-09-24 20:13:25 +0200

    Error messages

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -3,6 +3,27 @@ import logger from "../helpers/logging.js";
 import { Database } from "@azure/cosmos";
 import { getAppointments } from "../data/appointments.js";
 
+type RegisterFormData = {
+  first_name: string;
+  first_name_error?: string;
+  last_name: string;
+  last_name_error?: string;
+  email: string;
+  email_error?: string;
+  phone_number: string;
+  phone_number_error?: string;
+  residence: string;
+  residence_error?: string;
+  current_school: string;
+  current_school_error?: string;
+  current_class: string;
+  current_class_error?: string;
+  department: string;
+  department_error?: string;
+  appointment: string;
+  appointment_error?: string;
+};
+
 type RecaptchaResponse = {
   success: boolean;
   challenge_ts: string;
@@ -22,7 +43,10 @@ export function createRegistrationRoute(cosmosDb: Database): express.Router {
       return;
     }
 
-    res.render("registration", { captchaKey: process.env.RECAPTCHA_SITE_KEY, appointments: appointments.appointments });
+    res.render("registration", {
+      captchaKey: process.env.RECAPTCHA_SITE_KEY,
+      appointments: appointments.appointments,
+    });
   });
 
   router.post("/", async (req, res) => {
@@ -54,9 +78,48 @@ export function createRegistrationRoute(cosmosDb: Database): express.Router {
       return;
     }
 
-    const { first_name, last_name, email, phone_number, residence, current_school, current_class, department, appointment } = req.body;
+    const formData: RegisterFormData = req.body;
+    if (!formData.first_name) {
+      formData.first_name_error = "Bitte Vornamen eingeben";
+    }
+    if (!formData.last_name) {
+      formData.last_name_error = "Bitte Nachnamen eingeben";
+    }
+    if (!formData.email) {
+      formData.email_error = "Bitte E-Mail-Adresse eingeben";
+    }
+    if (!formData.phone_number) {
+      formData.phone_number_error = "Bitte Telefonnummer eingeben";
+    }
+    if (!formData.residence) {
+      formData.residence_error = "Bitte Wohnort eingeben";
+    }
+    if (!formData.current_school) {
+      formData.current_school_error = "Bitte aktuelle Schule eingeben";
+    }
+    if (!formData.current_class) {
+      formData.current_class_error = "Bitte aktuelle Klasse eingeben";
+    }
+    if (!formData.department) {
+      formData.department_error = "Bitte Abteilung auswählen";
+    }
+    if (!formData.appointment) {
+      formData.appointment_error = "Bitte Termin auswählen";
+    }
 
-    res.render("registration", { captchaKey: process.env.RECAPTCHA_SITE_KEY });
+    const appointments = await getAppointments(cosmosDb);
+    if (!appointments) {
+      logger.error("No appointments found");
+      res.status(500).send("No appointments found");
+      return;
+    }
+
+    formData.first_name_error = "Bitte Vornamen eingeben";
+    res.render("registration", {
+      captchaKey: process.env.RECAPTCHA_SITE_KEY,
+      appointments: appointments.appointments,
+      formData,
+    });
   });
 
   return router;
